2e34d1db2caf3ae5ed2b44bd43ca9b452cb841b4
Augusto Souza <augustosouza2222@gmail.com>
2020-12-29 13:57:17 -0300

    added html to js after calc

--- calculator.js
+++ calculator.js
@@ -4,7 +4,36 @@ let resultado = document.getElementById('res')
 function calc() {
 	console.log(numeroStreams.value)
 	if (numeroStreams.value > 0) {
-		console.log('número correto')
+		res.innerHTML = `<div class="container">
+			<div class="platforms spotify">
+				<img src="https://img.icons8.com/ios-filled/50/000000/spotify.png"/>
+				<h2>Spotify</h2>
+			</div>
+			<div class="platforms deezer">
+				<img src="https://img.icons8.com/ios-filled/50/000000/deezer.png"/>
+				<h2>Deezer</h2>
+			</div>
+			<div class="platforms amazon">
+				<img src="https://img.icons8.com/ios-filled/50/000000/amazon-music.png"/>
+				<h2>Amazon Music</h2>
+			</div>
+			<div class="platforms apple">
+				<img src="https://img.icons8.com/ios-filled/50/000000/mac-os.png"/>
+				<h2>Apple Music</h2>
+			</div>
+			<div class="platforms ytmusic">
+				<img src="https://img.icons8.com/ios-filled/50/000000/youtube-music.png"/>
+				<h2>YouTube Music</h2>
+			</div>
+			<div class="platforms tidal">
+				<img src="https://img.icons8.com/ios-filled/50/000000/tidal.png"/>
+				<h2>Tidal</h2>
+			</div>
+			<div class="platforms yt">
+				<img src="https://img.icons8.com/ios-filled/50/000000/amazon-music.png"/>
+				<h2>YouTube</h2>
+			</div>
+		</div>`
 	} else {
 		console.log('número errado')
 	}
